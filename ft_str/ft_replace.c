ff83c3567e1f55bd2cbc7e9c0efce4ab1b488861
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-05-12 22:38:12 +0100

    Code: fixing memory leak on ft_replace function.

--- ft_str/ft_replace.c
+++ ft_str/ft_replace.c
@@ -19,15 +19,13 @@ char	*ft_replace(char *str, char *old, char *new)
 	char	*ptr;
 	int		len;
 
-	if (!str || !old || !new)
+	if (!str || !old || !new || !ft_strnstr(str, old, ft_strlen(str)))
 		return (NULL);
 	len = ft_strlen(str) + ft_strlen(new) - ft_strlen(old);
 	ret = ft_calloc(len + 1, sizeof(char));
 	if (!ret)
 		return (NULL);
 	ptr = ft_strnstr(str, old, ft_strlen(str));
-	if (!ptr)
-		return (NULL);
 	tmp = ft_substr(str, 0, ptr - str);
 	ft_strlcat(ret, tmp, len + 1);
 	ft_strlcat(ret, new, len + 1);
