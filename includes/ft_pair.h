6e9f9bb96cf1e0e992f895f37d9b22a091ac8d1f
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-05-13 15:34:08 +0100

    Docs + norminette: documented pair module and checked norminette.

--- includes/ft_pair.h
+++ includes/ft_pair.h
@@ -26,16 +26,75 @@ typedef struct s_pair
 	void	*value;
 }t_pair;
 
+/**
+ * @brief Creates a new pair pointer with the given key and value
+ * 
+ * @param key The key to assign
+ * @param value The value to assign
+ * @return t_pair* A point to the new pair struct
+ * @return NULL If allocation fails
+ */
 t_pair	*ft_pair_new(void *key, void *value);
 
+/**
+ * @brief Duplicates the given pair into a newly allocated pair. If the 
+ * keycpy and valcpy functions are provided, they will be used to duplicate 
+ * the key and value respectively. If they are NULL, the key and value will
+ * be assigned directly to the new pair.
+ * 
+ * @param pair The pair to copy
+ * @param keycpy The function to copy the key
+ * @param valcpy The function to copy the value
+ * @return t_pair* A pointer to the new pair struct
+ * @return NULL If allocation fails
+ */
 t_pair	*ft_pair_copy(t_pair *pair, void *(*keycpy)(), void *(*valcpy)());
 
+/**
+ * @brief Swaps the key and value of the given pair into a newly allocated 
+ * pair. It uses the keycpy and valcpy functions to duplicate the key and
+ * value respectively. 
+ * 
+ * @note If the keycpy and valcpy functions are NULL, the key and value will
+ * be assigned directly to the new pair.
+ * @param pair The pair to swap
+ * @param keycpy The function to copy the key
+ * @param valcpy The function to copy the value
+ * @return t_pair* A pointer to the new pair struct
+ * @return NULL If allocation fails
+ */
 t_pair	*ft_pair_swap(t_pair *pair, void *(*keycpy)(), void *(*valcpy)());
 
+/**
+ * @brief Converts the given pair into a string using the provided keystr and
+ * valstr functions.
+ * 
+ * @param pair The pair to convert
+ * @param keystr The function to convert the key to a string
+ * @param valstr The function to convert the value to a string
+ * @return char* A pointer to the new string
+ * @return NULL If allocation fails
+ */
 char	*ft_pair_tostring(t_pair *pair, char *(*keystr)(), char *(*valstr)());
 
+/**
+ * @brief Frees the given pair using the provided keydel and valdel functions.
+ * @note If the keydel and valdel functions are NULL, the key and value will
+ * be freed (treated as primitive types that cannot be freed).  
+ 
+ * @param pair The pair to free 
+ * @param keydel The function to delete the key
+ * @param valdel The function to delete the value
+ */
 void	ft_pair_delete(t_pair *pair, void (*keydel)(), void (*valdel)());
 
+/**
+ * @brief Prints the given pair using the provided keystr and valstr functions.
+ * 
+ * @param pair The pair to print
+ * @param keystr The function to convert the key to a string
+ * @param valstr The function to convert the value to a string
+ */
 void	ft_pair_print(t_pair *pair, char *(*keystr)(), char *(*valstr)());
 
 #endif 
