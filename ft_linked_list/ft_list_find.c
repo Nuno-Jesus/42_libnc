467bf8ca9dd068a559a8208497b846a5ef7fef77
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-05-13 16:46:18 +0100

    Bug fix: fixing compilation error.

--- ft_linked_list/ft_list_find.c
+++ ft_linked_list/ft_list_find.c
@@ -18,7 +18,7 @@ t_list	*ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
 		return (NULL);
 	while (begin_list)
 	{
-		if (!(*cmp)(begin_list->data, data_ref))
+		if (!(*cmp)(begin_list->content, data_ref))
 			break ;
 		begin_list = begin_list->next;
 	}
