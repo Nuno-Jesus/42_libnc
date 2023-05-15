04d721c156ad3081c39ff62d3b4c67df68caa615
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-05-15 21:40:15 +0100

    Bug fix: fixed a bug where the dictionary copies wouldn't be copies due to returning the original dictionary. Fixed yet another bug where the setup of the delete function wouldn't occur in the copy.

--- ft_dictionary/ft_dict_copy.c
+++ ft_dictionary/ft_dict_copy.c
@@ -31,10 +31,10 @@ t_dict	*ft_dict_copy(t_dict *dict)
 	t_dict	*copy;
 
 	copy = ft_dict_new();
-	ft_dict_keys_setup(dict, dict->keycmp, dict->keycpy, dict->keydel);
-	ft_dict_values_setup(dict, dict->valcmp, dict->valcpy, dict->valdel);
+	ft_dict_keys_setup(copy, dict->keycmp, dict->keycpy, dict->keydel);
+	ft_dict_values_setup(copy, dict->valcmp, dict->valcpy, dict->valdel);
 	if (!copy)
 		return (NULL);
 	copy->pairs = helper(dict->pairs, dict->keycpy, dict->valcpy);
-	return (dict);
+	return (copy);
 }
